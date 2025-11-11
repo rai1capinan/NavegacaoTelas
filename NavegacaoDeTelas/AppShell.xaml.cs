30cfa8259ee2387d81285bb503f4ed407fc05c36
rai.csilva <rai.csilva@TIT0676433W11-1>
2025-11-11 17:18:16 -0300

    Adicionar arquivos de projeto.

---
+++ NavegacaoDeTelas/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NavegacaoDeTelas
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NavegacaoDeTelas/View/Login.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavegacaoDeTelas.View
+{
+    public partial class Login : ContentPage
+    {
+
+
+    }
+}
